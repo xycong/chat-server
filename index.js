00f28b1891530aae610801d212ce7a5a2c61546c
xycong <xycong@uwaterloo.ca>
2016-03-29 19:40:08 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('User connected');
+  io.emit('welcome', 'Welcome to the app');
+  socket.on('disconnect', function(){
+     console.log('User disconnected');
+     io.emit('goodbye', 'Goodbye');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
