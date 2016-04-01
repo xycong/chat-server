697aa5bfb9bf5dc9a4f3e4642e4b4d20d41fd0a9
xycong <xycong@uwaterloo.ca>
2016-04-01 14:26:23 -0400

    Deployment

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var root = __dirname + "/public";
 var accountSID = 'AC9f14a9f2f0055718cb19a493809b9a6c';
 var authToken = 'cd0a3a1c70c130d65b84a0778a8b80db'
-var accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTSzllZmMzMTcxOTdiZjkyYTA5NjE5ZWU5NTUyNDBjZWZiLTE0NTk1MjM4NjIiLCJpc3MiOiJTSzllZmMzMTcxOTdiZjkyYTA5NjE5ZWU5NTUyNDBjZWZiIiwic3ViIjoiQUM5ZjE0YTlmMmYwMDU1NzE4Y2IxOWE0OTM4MDliOWE2YyIsImV4cCI6MTQ1OTUyNzQ2MiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoicXVpY2tzdGFydCIsInJ0YyI6eyJjb25maWd1cmF0aW9uX3Byb2ZpbGVfc2lkIjoiVlM0MDNkYzY3OGY0MDllYzRkNTFlN2E5ODI1MmZlMTNlNiJ9fX0.QtA_Pm9P8m56CZ8YVSRjqoMuEJUGfkg7jgrmpaDjm8E";
 var twilio = require('twilio')(accountSID, authToken);
 var app = express();
 var http = require('http').Server(app);
@@ -31,7 +30,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // /* GET home page. */
 // router.get('/', function(req, res) {
